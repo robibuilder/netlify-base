2ac2e690ea90cf64e525834dab5b1012acc3cfff
Jacob Robinett <jrobinett@apple.com>
2022-03-14 11:57:08 -0500

    store api sync

--- src/app.js
+++ src/app.js
@@ -1,17 +1,52 @@
+
+
 const express = require('express');
 const serverless = require('serverless-http');
 
 const app = express();
 const router = express.Router();
 
-router.get('/', (req, res) => {
+const batteries = [
+    {
+        id: 1,
+        brand: 'Hart',
+        name: 'Hart 20V Battery Mount'
+    },
+    { 
+        id: 2,
+        brand: 'Craftsman',
+        name: 'Craftman V20* Battery Mount'
+    },
+    {
+        id: 3,
+        brand: 'DeWalt',
+        name: 'DEWALT 20V Battery Mount'
+    },
+    { 
+        id: 4,
+        brand: 'Black & Decker',
+        name: 'Black & Decker 20V Battery Mount'
+    },
+  ];
+
+router.get('/batteries', (req, res) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
     // res.json({
     //     'Hello': 'hi'
     // });
-    res.send("Hello");
+    res.status(200).json(batteries);
+});
+
+router.get('/batteries/:id', (req, res) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
+    let id = req.params.id;
+    let result = batteries.find((item) => item.id == id);
+
+    res.status(200).json(result);
 });
 
 router.get('/test', (req, res) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
     res.json({
         'Hello': 'hi'
     });
